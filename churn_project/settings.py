ed96c674ac9ff93c4ce46d4163452834a2484fca
Mothilal Chowdary <mothilalchowdary99@gmail.com>
2026-02-09 19:54:15 +0530

    Prepare project for deployment

--- churn_project/settings.py
+++ churn_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ul-1z(--a*q%4-4!fcu)!k8xj0gyg!fx%s%k=z)yp_fm$w(#h='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -115,4 +115,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
